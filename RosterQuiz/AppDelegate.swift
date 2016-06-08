ec3335b543d3b5f8b0c194ca36e9d7d4617ff963
Chris Gregg <tofergregg@gmail.com>
2016-06-08 07:27:58 -0400

    non working aerohttp

--- RosterQuiz/AppDelegate.swift
+++ RosterQuiz/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AeroGearOAuth2
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication,
+                     openURL url: NSURL,
+                             sourceApplication: String?,
+                             annotation: AnyObject) -> Bool {
+        let notification = NSNotification(name: AGAppLaunchedWithURLNotification,
+                                          object:nil,
+                                          userInfo:[UIApplicationLaunchOptionsURLKey:url])
+        NSNotificationCenter.defaultCenter().postNotification(notification)
+        return true
     }
 
 
--- RosterQuiz/SecondViewController.swift
+++ RosterQuiz/SecondViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import AeroGearHttp
+import AeroGearOAuth2
 
 class SecondViewController: UIViewController {
+    var http: Http!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.http = Http(baseURL: nil, sessionConfig: NSURLSessionConfiguration.defaultSessionConfiguration(), requestSerializer: JsonRequestSerializer(), responseSerializer: StringResponseSerializer())
+        self.http = Http()
+        share()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,50 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func share(){
+        let googleId = "795309004462-itm898hfu3gna8rnb1mjlv2jmaj8d6jd.apps.googleusercontent.com"
+        //let googleId = "aa851f2153089dc8f1269370828accabf4d6ebde"
+        let googleConfig = GoogleConfig(
+            clientId: googleId,                               // [1] Define a Google configuration
+            scopes:["https://www.googleapis.com/auth/drive"])            // [2] Specify scope
+        
+        let gdModule = AccountManager.addGoogleAccount(googleConfig)     // [3] Add it to AccountManager
+        self.http.authzModule = gdModule                                 // [4] Inject the AuthzModule
+        // into the HTTP layer object
+        
+        let param_list = ["q":"fullText+contains+'RosterQuiz_pics'"]
+        //let param_list = Dictionary<String,String>()
+
+        
+        self.http.request(.POST,path:"https://www.googleapis.com/drive/v2/files",
+            parameters: param_list,
+            completionHandler: {(response, error) in
+                if (error != nil) {
+                    self.presentAlert("Error", message: error!.localizedDescription)
+                } else {
+                    // response is json
+                    let responseDict = response as! Dictionary<String,AnyObject>
+                    
+                    print(responseDict["kind"] as! String)
+                    print(responseDict["etag"] as! String)
+                    print(responseDict["selfLink"] as! String)
+                    if (responseDict["nextPageToken"] != nil) {
+                        print(responseDict["nextPageToken"] as! String)
+                    }
+                    if (responseDict["nextLink"] != nil) {
+                        print(responseDict["nextLink"] as! String)
+                    }
+
+                    self.presentAlert("Success", message: (response?.string)!)
+                }
+        })
+    }
+    
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
