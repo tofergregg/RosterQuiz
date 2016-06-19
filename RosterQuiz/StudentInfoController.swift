5575c149de9376e4cc07c38e4c4cfd808e16eb58
Chris Gregg <tofergregg@gmail.com>
2016-06-19 13:17:00 -0400

    added keyboard handling to student info

--- RosterQuiz/StudentInfoController.swift
+++ RosterQuiz/StudentInfoController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class StudentInfoController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class StudentInfoController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
     
     var student : Student! = nil
     
@@ -56,6 +56,97 @@ class StudentInfoController : UIViewController, UIImagePickerControllerDelegate,
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return false to ignore.
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(sender : UITextField)
+    {
+        var offset : CGFloat = 0
+        if sender == lastNameText {
+            offset = 100.0
+        }
+        else if sender == firstNameText {
+            offset = 140.0
+        }
+        else if sender == yearText {
+            offset = 180.0
+        }
+        //move the main view, so that the keyboard does not hide it.
+        if  (self.view.frame.origin.y >= 0)
+        {
+            self.setViewMovedUp(true,kOFFSET_FOR_KEYBOARD: offset);
+        }
+    }
+    
+    func textFieldDidEndEditing(sender : UITextField)
+    {
+        var offset : CGFloat = 0
+        if sender == lastNameText {
+            offset = 100.0
+        }
+        else if sender == firstNameText {
+            offset = 140.0
+        }
+        else if sender == yearText {
+            offset = 180.0
+        }
+        
+        //move the main view, so that the keyboard does not hide it.
+        if  (self.view.frame.origin.y < 0)
+        {
+            self.setViewMovedUp(false,kOFFSET_FOR_KEYBOARD: offset);
+        }
+    }
+    
+    func textViewDidBeginEditing(sender : UITextView) {
+        //move the main view, so that the keyboard does not hide it.
+        if  (self.view.frame.origin.y >= 0)
+        {
+            self.setViewMovedUp(true, kOFFSET_FOR_KEYBOARD: 80.0)
+            
+            // add done button
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(StudentInfoController.dismiss))
+        }
+    }
+    
+    func dismiss() {
+        //move the main view, so that the keyboard does not hide it.
+        if  (self.view.frame.origin.y < 0)
+        {
+            self.setViewMovedUp(false, kOFFSET_FOR_KEYBOARD: 80.0)
+        }
+        notesText.resignFirstResponder()
+        // remove done button
+        self.navigationItem.rightBarButtonItem = nil
+    }
+    
+    //method to move the view up/down whenever the keyboard is shown/dismissed
+    func setViewMovedUp(movedUp : Bool, kOFFSET_FOR_KEYBOARD : CGFloat) {
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(0.3)
+    
+        var rect = self.view.frame;
+        if (movedUp)
+        {
+        // 1. move the view's origin up so that the text field that will be hidden come above the keyboard
+        // 2. increase the size of the view so that the area behind the keyboard is covered up.
+            rect.origin.y -= kOFFSET_FOR_KEYBOARD;
+            //rect.size.height += kOFFSET_FOR_KEYBOARD;
+        }
+        else
+        {
+            // revert back to the normal state.
+            rect.origin.y += kOFFSET_FOR_KEYBOARD;
+            //rect.size.height -= kOFFSET_FOR_KEYBOARD;
+        }
+        self.view.frame = rect;
+        
+        UIView.commitAnimations();
     }
     
     func nameChanged(){
